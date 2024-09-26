4d5c20f4278256cf2017293449e67fd3f7f0eea7
DanaAniskovich <144224905+DanaAniskovich@users.noreply.github.com>
2024-09-26 18:22:48 +0300

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,8 @@
+public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if (i >= 10 && i % 2 == 0) {
+                System.out.println(i); 
+            }
+        }
+    }
+}
